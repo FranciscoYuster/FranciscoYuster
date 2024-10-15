784f7d1a6e30169b81536352dba64e5c6df60b71
FranciscoYuster <yusterfrancisco@gmail.com>
2024-10-15 17:28:50 -0300

    style 22

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-function showSection(sectionId) {
-    // Ocultar todas las secciones
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-      section.classList.remove('active');
-    });
-  
-    // Mostrar solo la sección seleccionada
-    const selectedSection = document.getElementById(sectionId);
-    selectedSection.classList.add('active');
+// script.js
+function navigateTo(page) {
+    // Redirecciona a la página seleccionada
+    window.location.href = `${page}.html`;
   }
-  
-  // Mostrar la sección de inicio al cargar la página
-  document.addEventListener("DOMContentLoaded", function() {
-    showSection('inicio');
-  });
   
