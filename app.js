3d93b431cef6a3da5b63d3f5282b421503669fba
FranciscoYuster <yusterfrancisco@gmail.com>
2024-10-16 16:36:31 -0300

    style 50

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 // Función para cargar una sección HTML en el <main>
 function loadSection(section) {
-    fetch(`${section}.html`)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => console.error('Error al cargar la sección:', error));
+    const content = document.getElementById('content');
+    
+    // Agrega la clase para el efecto de desvanecimiento
+    content.classList.add('fade-out');
+    
+    // Espera a que la animación de desvanecimiento se complete antes de cambiar el contenido
+    setTimeout(() => {
+        fetch(`${section}.html`)
+            .then(response => response.text())
+            .then(data => {
+                content.innerHTML = data;
+                
+                // Reinicia las clases de desvanecimiento
+                content.classList.remove('fade-out');
+                content.classList.add('fade-in');
+                
+                // Desplazamiento suave hacia arriba
+                window.scrollTo({
+                    top: 100, // Ajusta este valor para el desplazamiento deseado
+                    behavior: 'smooth'
+                });
+                
+                // Quita la clase fade-in al finalizar la transición
+                setTimeout(() => {
+                    content.classList.remove('fade-in');
+                }, 300); // Asegúrate de que coincida con la duración de la transición
+            })
+            .catch(error => console.error('Error al cargar la sección:', error));
+    }, 300); // Ajusta el tiempo de espera para coincidir con el tiempo de desvanecimiento
 }
 
 // Detecta el clic en los enlaces de navegación
